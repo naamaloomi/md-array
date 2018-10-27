97a502967df5380ffbdfa6b1d232b4e4b90ed7f6
Amir Sadeghin <amir.sadeghin@gmail.com>
2018-10-27 21:46:58 +0200

    Added Equals to Propakrav.

--- MDArray/PropaKrav.cs
+++ MDArray/PropaKrav.cs
@@ -15,6 +15,11 @@
         public static PropaKrav ObligatoriskPerMotpart => new PropaKrav(2, "Obligatorisk per motpart");
         public static PropaKrav ObligatoriskPerGrupp => new PropaKrav(3, "Obligatorisk per grupp");
 
+        public bool Equals(PropaKrav krav)
+        {
+            return krav.Type == Type;
+        }
+
         public override string ToString()
         {
             return Name;
