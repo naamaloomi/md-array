4977cd3c45f8c5408a4702090c84f3cb83c5223c
Amir Sadeghin <amir.sadeghin@gmail.com>
2018-10-27 13:08:12 +0200

    Added Regelverk.

---
+++ MDArray/PropaKrav.cs
@@ -0,0 +1,22 @@
+﻿namespace MDArray
+{
+    internal class PropaKrav
+    {
+        public PropaKrav(int type)
+        {
+            Type = type;
+        }
+
+        public int Type { get; }
+
+        public static PropaKrav Valfri => new PropaKrav(1);
+        public static PropaKrav ObligatoriskPerMotpart => new PropaKrav(2);
+        public static PropaKrav ObligatoriskPerGrupp => new PropaKrav(3);
+
+        public new bool Equals(object obj)
+        {
+            var krav = obj as PropaKrav;
+            return krav?.Type == Type;
+        }
+    }
+}
---
+++ MDArray/Regelverk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MDArray
+{
+    internal class Regelverk
+    {
+        // Definiera regler som delegater. 
+        private static readonly Datcha.Del RegelA = Datcha.RegelA;
+
+        private static readonly Datcha.Del RegelB = Datcha.RegelB;
+        private static readonly Datcha.Del RegelC = Datcha.RegelC;
+
+        // Definiera regelverk.
+        private static readonly Dictionary<(Motpart, RiskGrupp), Delegate> Rules =
+            new Dictionary<(Motpart, RiskGrupp), Delegate>
+            {
+                {(Motpart.Brf, RiskGrupp.A), RegelA},
+                {(Motpart.Brf, RiskGrupp.B), RegelA},
+                {(Motpart.Brf, RiskGrupp.C), RegelB},
+                {(Motpart.FastighetsBolag, RiskGrupp.A), RegelB},
+                {(Motpart.FastighetsBolag, RiskGrupp.B), RegelC},
+                {(Motpart.FastighetsBolag, RiskGrupp.C), RegelC},
+                {(Motpart.Ovriga, RiskGrupp.A), RegelA},
+                {(Motpart.Ovriga, RiskGrupp.B), RegelB},
+                {(Motpart.Ovriga, RiskGrupp.C), RegelC}
+            };
+
+        public static Delegate GetRegel(Motpart motpart, RiskGrupp riskGrupp)
+        {
+            return Rules[(motpart, riskGrupp)];
+        }
+    }
+}
